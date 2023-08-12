bc075172b5eb294483f2f58d204e493450debf10
wencharles <ndanyicharles14@gmail.com>
2023-08-12 08:58:59 +0300

    Added github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,9 +18,9 @@ const Footer = () => {
           className="mt-3 text-secondary text-[17px] loading-[30px] max-w-3xl flex gap-10"
         >
           <div className="flex gap-5">
-            <a href="https://github.com/Ze7Hu" className="h-10 w-10"><img src={github} alt="facebook"></img></a>
+            <a href="https://github.com/wencharles" className="h-10 w-10"><img src={github} alt="facebook"></img></a>
             
-            <a href="https://instagram.com/hiidz1?igshid=OGQ5ZDc2ODk2ZA==" className="h-10 w-10"><img src={ig} alt="instagram"></img></a>
+            <a href="#" className="h-10 w-10"><img src={ig} alt="instagram"></img></a>
             <a href="#" className="h-10 w-10"><img src={twitter} alt="twitter"></img></a>
             {/* <a href="#" className="h-10 w-10"><img src={skype} alt="skype"></img></a> */}
           </div>
